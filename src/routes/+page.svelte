54534521c2f440756b38f9b04e29784b2278fb1c
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-05-25 14:01:13 +0530

    feat: add `menu-bar` and "About Me" Section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex min-h-1000 justify-center">
+	<div class="window w-full lg:w-3/4">
+		<div class="window">
+			<ul role="menu-bar" class="flex justify-center">
+				<li role="menu-item" tabindex="0" aria-haspopup="false">home</li>
+				<li role="menu-item" tabindex="0" aria-haspopup="false">projects</li>
+				<li role="menu-item" tabindex="0" aria-haspopup="false">blog</li>
+				<li role="menu-item" tabindex="0" aria-haspopup="false">contact</li>
+			</ul>
+		</div>
+
+		<div class="px-4 pt-0.5 pb-4">
+			<h2>About Me</h2>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius, nisi nec euismod euismod, ligula nibh ultricies mauris, sit amet rutrum mi dolor quis leo. Sed vehicula mi vitae varius iaculis. Praesent eu lorem dignissim, vehicula ex condimentum, sodales justo. Vivamus dignissim quis tellus vel euismod. Nullam cursus erat id lectus tristique placerat. Fusce facilisis lectus eget semper ultricies. Cras vitae tincidunt lacus. Curabitur ut odio tortor. Nulla id fermentum urna. Cras molestie, neque nec condimentum tincidunt, nisl orci feugiat odio, sed dictum dolor mi in libero. Nulla nec ante consectetur, dictum massa eget, aliquam magna. Donec orci metus, vulputate a ipsum ut, aliquet efficitur tortor. Aliquam erat volutpat. </p>
+		</div>
+	</div>
+</div>
