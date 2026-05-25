53777330b3f09ffbd14cb202694296f9d01a06e2
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-05-25 14:55:19 +0530

    feat: add sample experience in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script lang="ts">
+    import ExperienceCard from '$lib/components/experience-card.svelte';
+</script>
 <div class="flex min-h-1000 justify-center">
 	<div class="window w-full lg:w-3/4">
 		<div class="window">
@@ -12,6 +15,48 @@
 		<div class="px-4 pt-0.5 pb-4">
 			<h2>About Me</h2>
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius, nisi nec euismod euismod, ligula nibh ultricies mauris, sit amet rutrum mi dolor quis leo. Sed vehicula mi vitae varius iaculis. Praesent eu lorem dignissim, vehicula ex condimentum, sodales justo. Vivamus dignissim quis tellus vel euismod. Nullam cursus erat id lectus tristique placerat. Fusce facilisis lectus eget semper ultricies. Cras vitae tincidunt lacus. Curabitur ut odio tortor. Nulla id fermentum urna. Cras molestie, neque nec condimentum tincidunt, nisl orci feugiat odio, sed dictum dolor mi in libero. Nulla nec ante consectetur, dictum massa eget, aliquam magna. Donec orci metus, vulputate a ipsum ut, aliquet efficitur tortor. Aliquam erat volutpat. </p>
+
+			<h2>Experience</h2>
+			<ExperienceCard
+				company="Corp 1"
+				logoUrl="https://via.placeholder.com/80"
+				role="Software Engineer"
+				startDate="Jan 2020"
+				endDate="Dec 2023"
+				description={[
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+					"Aliquam dictum est convallis tristique sagittis.",
+					"Vivamus euismod lacus ut metus interdum, vitae interdum elit congue."
+				]}
+				techStack={["React", "TypeScript", "Node.js", "PostgreSQL"]}
+			/>
+
+			<ExperienceCard
+                company="Corp 2"
+                logoUrl="https://via.placeholder.com/80"
+                role="Frontend Developer"
+                startDate="Jan 2018"
+                endDate="Dec 2019"
+                description={[
+                    "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                ]}
+                techStack={["Vue.js", "JavaScript", "CSS", "HTML"]}
+            />
+
+			<ExperienceCard
+                company="Corp 3"
+                logoUrl="https://via.placeholder.com/80"
+                role="Intern"
+                startDate="Jun 2017"
+                endDate="Aug 2017"
+                description={[
+                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                ]}
+                techStack={["Python", "Django", "SQLite"]}
+            />
+
 		</div>
 	</div>
 </div>
