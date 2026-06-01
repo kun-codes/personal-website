6fa0453b9dcef53098ba7af684d7249e27898216
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-06-01 20:09:24 +0530

    feat: add `/blogs` page

---
+++ src/routes/blogs/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { resolve } from '$app/paths';
+</script>
+
+<div class="standard-dialog min-w-0! ">
+	<ul role="menu-bar" class="flex flex-wrap justify-center">
+		<li role="menu-item" tabindex="0" aria-haspopup="false"><a href={resolve("/")}>home</a></li>
+		<li role="menu-item" tabindex="0" aria-haspopup="false"><a href={resolve("/#contact-me")}>contact</a></li>
+	</ul>
+</div>
+
+<div>
+	<h2>Blog</h2>
+	<p>Welcome to my blog!</p>
+</div>
