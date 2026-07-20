c7bbdf464e4e2d8b74ebf30240c7fa8d7de81b06
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-07-21 00:29:58 +0530

    style: remove footer section from layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,6 @@
 <div class="flex justify-center p-2 lg:p-4 lg:px-0">
 	<div class="w-full lg:w-3/4">
 		{@render children()}
-		<div class="details-bar mt-4">
-			<span>© 2026 Bishwa Bhusan Saha</span>
-			<span class="flex gap-4">
-				<a href="https://github.com/kun-codes/personal-website/blob/main/LICENSE" class="underline">MIT</a>
-			</span>
-		</div>
 	</div>
 </div>
 
