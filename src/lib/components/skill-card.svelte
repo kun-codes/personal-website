dc710d4c5cc0bd4c85dde6b41f1b01064f34a1fd
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-07-21 22:47:25 +0530

    style: replace `window` class with `border-1` in `SkillCard` component

--- src/lib/components/skill-card.svelte
+++ src/lib/components/skill-card.svelte
@@ -8,7 +8,7 @@
 	} = $props();
 </script>
 
-<div class="window flex flex-col items-center min-w-0! m-0!">
+<div class="border-1 flex flex-col items-center min-w-0! m-0!">
 	<div class="flex flex-col items-center justify-center gap-2 p-4">
 		<img src={imageUrl} alt={name} class="w-16 h-16 object-contain" />
 		<p class="m-0 text-sm text-center">{name}</p>
